c015a9797dff4c1b4df46d8855c559c221ab883b
digitaldonkey <tho@donkeymedia.eu>
2024-12-08 09:59:41 +0100

    ci

--- integration_tests/settings.js
+++ integration_tests/settings.js
@@ -1,5 +1,5 @@
 module.exports = {
-    domain: 'open-source-event-calendar.test',
+    domain: 'http://open-source-event-calendar.test',
     screenshotsDir: '/tmp/integration_test_results', // No trailing slash
     headless: true,
     screen: {
