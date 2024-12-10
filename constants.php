ccb7c937bbd20e85430a53d92f66f74c0ee8599a
digitaldonkey <tho@donkeymedia.eu>
2024-12-10 14:25:34 +0100

    Added Tag release to pipeline.

--- constants.php
+++ constants.php
@@ -48,7 +48,7 @@ function osec_initiate_constants($osec_base_dir, $osec_base_url)
     // = Plugin Version =
     // ==================
     if ( ! defined('OSEC_VERSION')) {
-        define('OSEC_VERSION', '0.9');
+        define('OSEC_VERSION', '0.9.0');
     }
 
     // =================
