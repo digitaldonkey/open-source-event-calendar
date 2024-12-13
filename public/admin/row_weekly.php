717de41d13dd33257e74814cdea558f4d54287a8
digitaldonkey <tho@donkeymedia.eu>
2024-12-13 09:52:22 +0100

    Remove unnecessary unsecure function.

--- public/admin/row_weekly.php
+++ public/admin/row_weekly.php
@@ -12,7 +12,7 @@
 </div>
 <div class="ai1ec-form-group">
     <label class="ai1ec-control-label ai1ec-col-sm-3">
-        <?php _ex('On', 'Recurrence editor - weekly tab', 'open-source-event-calendar'); ?>:
+        <?php echo _x('On', 'Recurrence editor - weekly tab', 'open-source-event-calendar'); ?>:
     </label>
     <div class="ai1ec-col-sm-9">
         <?php echo $week_days; ?>
--- public/admin/row_yearly.php
+++ public/admin/row_yearly.php
@@ -13,7 +13,7 @@
 
 <div class="ai1ec-form-group">
     <label for="osec_yearly_date_select" class="ai1ec-control-label ai1ec-col-sm-3">
-        <?php _ex('In', 'Recurrence editor - yearly tab', 'open-source-event-calendar'); ?>:
+        <?php echo _x('In', 'Recurrence editor - yearly tab', 'open-source-event-calendar'); ?>:
     </label>
     <div class="ai1ec-col-sm-9">
         <?php echo $year; ?>
