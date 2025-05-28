b907229ce804781c95adf1f9c9565353b41252f9
digitaldonkey <tho@donkeymedia.eu>
2025-05-28 11:39:36 +0200

    Bump Version 1.0.3

--- open-source-event-calendar.php
+++ open-source-event-calendar.php
@@ -12,7 +12,7 @@
  * Requires at least: 6.6
  * Tested up to: 6.7.2
  * Requires PHP: 8.2
- * Stable Tag: 1.0.2
+ * Stable Tag: 1.0.3
  * License: GPL-3.0-or-later
  * License URI: https://www.gnu.org/licenses/gpl-3.0.html
  * Text Domain: open-source-event-calendar
