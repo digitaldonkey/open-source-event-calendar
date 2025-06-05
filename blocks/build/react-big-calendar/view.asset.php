14cdf595aa0c57006b5fc69ece4264f79d67adf7
digitaldonkey <tho@donkeymedia.eu>
2025-06-05 11:36:52 +0200

    WIP

--- blocks/build/react-big-calendar/view.asset.php
+++ blocks/build/react-big-calendar/view.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-api-fetch', 'wp-element', 'wp-url'), 'version' => 'ed0cd7b72d0365c58e98');
+<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-api-fetch', 'wp-element', 'wp-url'), 'version' => '01f2510cb3332499c894');
