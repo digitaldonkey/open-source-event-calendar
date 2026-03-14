0f255372c0f76fbde28e8b6f402266fc15920657
Thorsten Krug <tho@donkeymedia.eu>
2026-03-14 01:53:30 +0100

    Evaluate deployment. WIP.

--- .circleci/deploy-wp-plugin-release.sh
+++ .circleci/deploy-wp-plugin-release.sh
@@ -53,14 +53,14 @@ if [[ $CIRCLE_TAG != $LATEST_GIT_TAG ]]; then
 #    exit 1
 fi
 TEST_TAG="1.0.11"
-if [[ $TEST_TAG =~ ^\d+\.\d+\.\d+$ ]]; then
+if [[ $TEST_TAG =~ ^[[:digit:]]+\.[[:digit:]]+\.[[:digit:]]+\.$ ]]; then
     echo "TEST_TAG is a semantic tag." 1>&2
 #    exit 1
   else
     echo "TEST_TAG is NOT semantic tag." 1>&2
 fi
 
-if [[ "$LATEST_GIT_TAG" =~ ^\d+\.\d+\.\d+$ ]]; then
+if [[ LATEST_GIT_TAG =~ ^[[:digit:]]+\.[[:digit:]]+\.[[:digit:]]+\.$ ]]; then
     echo "LATEST_GIT_TAG:$LATEST_GIT_TAG is a semantic tag." 1>&2
 #    exit 1
   else
