6ba4f9fd73d14d2b0efe598e3bf7179c9a94cf9c
Thorsten Krug <tho@donkeymedia.eu>
2026-06-07 18:48:54 +0200

    Understand Line ending handling better.

--- src/App/Model/IcsImportExportParser.php
+++ src/App/Model/IcsImportExportParser.php
@@ -410,7 +410,7 @@ class IcsImportExportParser extends OsecBaseClass implements ImportExportParserI
                     'post_content'   => stripslashes(
                         str_replace(
                             '\n',
-                            "\n",
+                            '<br />',
                             $e->getDescription()
                         )
                     ),
