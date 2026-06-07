1149daa5728bf6a7363fafbe0cea538ffe2d1b62
Thorsten Krug <tho@donkeymedia.eu>
2026-06-07 18:13:56 +0200

    Remove `Add to timely` from Subscribe options. No one needs that. Sorry.

--- src/App/View/Calendar/CalendarSubscribeButtonView.php
+++ src/App/View/Calendar/CalendarSubscribeButtonView.php
@@ -23,17 +23,12 @@ class CalendarSubscribeButtonView extends OsecBaseClass
     {
         return [
             'label' => [
-                'timely'    => __('Add to Timely Calendar', 'open-source-event-calendar'),
                 'google'    => __('Add to Google', 'open-source-event-calendar'),
                 'outlook'   => __('Add to Outlook', 'open-source-event-calendar'),
                 'apple'     => __('Add to Apple Calendar', 'open-source-event-calendar'),
                 'plaintext' => __('Add to other calendar', 'open-source-event-calendar'),
             ],
             'title' => [
-                'timely'    => __(
-                    'Copy this URL for your own Timely calendar or click to add to your rich-text calendar',
-                    'open-source-event-calendar'
-                ),
                 'google'    => __('Subscribe to this calendar in your Google Calendar', 'open-source-event-calendar'),
                 'outlook'   => __('Subscribe to this calendar in MS Outlook', 'open-source-event-calendar'),
                 'apple'     => __('Subscribe to this calendar in Apple Calendar/iCal', 'open-source-event-calendar'),
